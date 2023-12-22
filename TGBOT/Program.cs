902b4e60f15c237608841fdd25f5572225d7e1e0
Тимофей <timone300003@gmail.com>
2023-12-23 00:49:58 +0300

    need help

--- TGBOT/Program.cs
+++ TGBOT/Program.cs
@@ -1,9 +1,47 @@
-﻿using System;
+﻿
 
-class Bot 
+using System.Net.Http;
+
+string token = File.ReadAllText("token.config");
+HttpClient hc = new();
+hc.BaseAddress = new Uri($"https://api.telegram.org/bot{token}/");
+
+string ContentObj = hc.GetStringAsync("getme").Result;
+System.Console.WriteLine(ContentObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class GetMesModel
 {
-    public static void Main(string[] args)
-    {
+    bool ok;
+    ResultModel result;
 
-    }
+}
+
+class ResultModel
+{
+    long id;
+    bool is_bot;
+    string first_name;
+    string username;
+    bool can_join_groups;
+    bool can_read_all_group_messages;
+    bool supports_inline_queries;
 }
